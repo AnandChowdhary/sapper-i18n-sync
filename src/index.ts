e621f2d9c80ca3625c66a688f63c193419df3d92
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-31 13:21:35 +0530

    :sparkles: Add CLI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import { argv } from "yargs";
+import { options } from "yargs";
+import { generateLocales } from "./generate-locales";
+import { generateSvelte } from "./generate-svelte";
 
-if (process.argv[2] === "generate-locales") {
-  console.log(args);
+const argv = options({
+  gitRepo: { type: "string" },
+  path: { type: "string" },
+  tempDir: { type: "string" },
+  defaultLang: { type: "string" },
+  cacheBust: { type: "boolean" },
+}).argv;
+
+if (argv._[0] === "generate-locales") {
+  if (argv.gitRepo && argv.path)
+    generateLocales({
+      gitRepo: argv.gitRepo,
+      path: argv.path,
+      tempDir: argv.tempDir,
+      defaultLang: argv.defaultLang,
+      cacheBust: argv.cacheBust,
+    });
+  else throw new Error("Path and git repo are required");
+} else if (argv._[0] === "generate-svelte") {
+  generateSvelte();
 }

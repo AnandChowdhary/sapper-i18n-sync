049e70476b744619b968106c600b3621ff5e179b
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-31 12:24:53 +0530

    :sparkles: Generate locales

--- src/generate-locales.ts
+++ src/generate-locales.ts
@@ -5,8 +5,34 @@ import { remove, copy, pathExists } from "fs-extra";
 export interface GenerateLocalesParams {
   gitRepo: string;
   path: string;
+  tempDir?: string;
+  defaultLang?: string;
+  cacheBust?: boolean;
 }
 
+/**
+ * Generate locales from a remote git repo
+ * @param params - Options
+ */
 export const generateLocales = async (params: GenerateLocalesParams) => {
-  join("");
+  const tempDir = params.tempDir ?? "__i18n-temp";
+
+  // Use cached translations
+  if (
+    !params.cacheBust &&
+    (await pathExists(
+      join(
+        ".",
+        tempDir,
+        params.defaultLang ? `${params.defaultLang}.json` : "en-ch.json"
+      )
+    ))
+  )
+    return;
+  await remove(join(".", "locales"));
+
+  console.log("Generating i18n files...");
+  await remove(join(".", tempDir));
+  await execa(`git clone --depth 1 ${params.gitRepo} ${tempDir}`);
+  await copy(join(".", tempDir, "locales"), join(".", "locales"));
 };
